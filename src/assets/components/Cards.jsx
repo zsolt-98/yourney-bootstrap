2f623a66f395ada4339639259de8c6178adea07c
zsolt-98 <zsolt.nagy998@gmail.com>
2024-09-24 19:57:57 +0200

    static cards finished

--- src/assets/components/Cards.jsx
+++ src/assets/components/Cards.jsx
@@ -8,14 +8,14 @@ export default function Cards() {
         >
           <div className="d-flex  flex-column  gap-4 text-light text-center">
             <h3 className="display-2 fw-semibold ">
-              When speed meets sophistication
+              Where speed meets sophistication
             </h3>
             <p className="h2 fw-light">
               Fast, easy bookings for an extraordinary driving experience
             </p>
           </div>
-          <div className="row g-5 mt-8 justify-content-evenly">
-            <div className="col-lg-4 card bg-primary pb-5 px-6 text-light align-items-center  text-center">
+          <div className="d-flex gap-6 flex-wrap justify-content-center mt-8">
+            <div className="cards__card card bg-primary p-5 text-light align-items-center justify-content-evenly gap-3 text-center ">
               <div className="card__image--container">
                 <img
                   src="src/assets/images/card-select-car.png"
@@ -29,32 +29,33 @@ export default function Cards() {
                 elevate your journey with unmatched performance and style
               </p>
             </div>
-            <div className="col-lg-4 card bg-secondary pb-5 px-6 text-dark align-items-center gap-3 text-center">
+            <div className="cards__card card bg-secondary p-5 text-dark align-items-center justify-content-evenly gap-3 text-center">
               <div className="card__image--container">
                 <img
-                  src="src/assets/images/card-select-car.png"
+                  src="src/assets/images/card-select-date.png"
                   className="w-75 "
                   alt=""
                 />
               </div>
-              <h4 className="card-title h1">Select Vehicle</h4>
+              <h4 className="card-title h1">Schedule Dates</h4>
               <p className="card-text h4">
-                Choose from our exclusive range of luxury vehicles, tailored to
-                elevate your journey with unmatched performance and style
+                Select your rental dates with ease to match your travel plans
+                perfectly. Enjoy a smooth booking experience from start to
+                finish!
               </p>
             </div>
-            <div className="col-lg-4 card  bg-secondary pb-5 px-6 text-dark align-items-center gap-3 text-center">
+            <div className="cards__card card bg-light p-5 text-dark align-items-center justify-content-evenly gap-3 text-center">
               <div className="card__image--container">
                 <img
-                  src="src/assets/images/card-select-car.png"
+                  src="src/assets/images/card-hit-the-road.png"
                   className="w-75"
                   alt=""
                 />
               </div>
-              <h4 className="card-title h1">Select Vehicle</h4>
+              <h4 className="card-title h1">Hit the road</h4>
               <p className="card-text h4">
-                Choose from our exclusive range of luxury vehicles, tailored to
-                elevate your journey with unmatched performance and style
+                All set! Time to hit the road and make unforgettable memories.
+                Each mile brings new experiences just waiting to be discovered.
               </p>
             </div>
           </div>
